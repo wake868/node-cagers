8f02101954774ebee0a514bf1b9d184bf14b3a2b
Mike Wakeland <wake868@gmail.com>
2015-11-19 13:02:17 -0800

    added data driven pages

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk(process.env.MONGOLAB_URI);
-//var db = monk('cagers:ki7hep7$@ds057204.mongolab.com:57204/heroku_hjdp5nhl');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
