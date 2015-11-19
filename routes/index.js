55fa69e39c0a9f7fbaf139e7658d525441585448
Mike Wakeland <wake868@gmail.com>
2015-11-19 12:44:37 -0800

    added data driven pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cagers' });
+  var db = req.db;
+  var collection = db.get('articlecollection');
+  collection.find({"slug":"philosophy"},{},function(e,docs){
+      res.render('cagers/article', {
+          "article" : docs,
+          "title" : docs[0].title
+      });
+  });
 });
 
 router.get('/article/:slug', function(req, res, next) {
